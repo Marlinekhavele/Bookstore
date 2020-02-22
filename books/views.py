6e7bf2cdbb89836f8ac517048796bed7bd25fc0a
MarlineKhavele <marlinekhavele6@gmail.com>
2020-02-22 22:43:59 +0300

    Added search

--- books/views.py
+++ books/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.mixins import (
 )
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-
+from django.db.models import Q
 from .models import Book
 
 # Create your views here.
@@ -21,3 +21,20 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = "books/book_detail.html"
     login_url = "account_login"
     permission_required = "books.special_status"
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
+        # return Book.objects.filter(#hardcoding values
+        #     Q(title__icontains="gravity") | Q(title__icontains="vegan recipes")
+        # )
+
+    # queryset = Book.objects.filter(title__icontains="Gravity")
